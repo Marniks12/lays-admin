1943e5b4e88536dc9e6ab842f510751e5a15e018
Marvis Ojo <114944409+Marniks12@users.noreply.github.com>
2026-01-08 20:10:35 +0100

    fix: correct LoginView casing for linux

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LoginView from "../views/loginView_tmp.vue"
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
